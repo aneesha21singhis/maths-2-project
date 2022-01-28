6c933b2ae176e89e30ff11623f2d3c55295fd778
aneesha21singhis <92245422+aneesha21singhis@users.noreply.github.com>
2022-01-28 16:09:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function addUser(){
+    player1_name = document.getElementById("player1_name").value;
+    player2_name = document.getElementById("player2_name").value; 
+    console.log
+
+    localStorage.setItem("plyer1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+    console.log
+
+    window.location.replace("game_page.html");
+    console.log
+}   
